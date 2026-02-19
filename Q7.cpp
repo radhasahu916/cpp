0061171794174908603319bdcc3884427acc1841
radhasahu916 <radhasahu5657@gmail.com>
2026-02-19 19:55:52 +0530

    Add files via upload

---
+++ Q7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+float temperature,F,c;
+cout<<"enter temperature in celsius";
+cin>> c;
+
+F=(9.0/5.0)*c+32;
+cout<<"temperature in">>F;
+
+
+
+}
