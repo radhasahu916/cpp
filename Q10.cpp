1dacbd33f47b9d43eef4601b18d136f3d1b04d4e
radhasahu916 <radhasahu5657@gmail.com>
2026-02-19 23:34:30 +0530

    Add files via upload

---
+++ Q10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+float a,b;
+cout<<"enter two number";
+cin>>a>>b;
+cout<<"addition"<<a+a<<endl;
+cout<<"subtraction"<<a-b<<endl;
+cout<<"multiplication"<<a*b<<endl;
+cout<<"division"<<a/b<<endl;
+return 0;
+
+
+
+}
