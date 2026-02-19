3b09d2cc1d6ca88ba5d5e5dc10b7b8c90cd8f555
radhasahu916 <radhasahu5657@gmail.com>
2026-02-19 23:34:00 +0530

    Add files via upload

---
+++ Q9.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+cout<<"size of int="<<
+sizeof(int)<<"bytes\n";
+cout<<"size of float="<<
+sizeof(float)<<"bytes\n";
+cout<<"size of double="<<
+sizeof(double)<<"bytes\n";
+cout<<"size of char="<<
+sizeof(char)<<"bytes\n";
+return 0;
+
+
+
+}
