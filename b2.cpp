4e29aebe86e41b4e720efa59c37884d2017feafc
radhasahu916 <radhasahu5657@gmail.com>
2026-02-20 22:52:49 +0530

    Add files via upload

---
+++ b2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"enter the value a=";
+cin>>a;
+cout<<"enter the value b=";
+cin>>b;
+a=a+b;
+b=a-b;
+a=a-b;
+cout<<"a"<<a<<endl;
+cout<<"b"<<b;
+
+
+
+
+}
