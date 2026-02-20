7a2ddc07d4a583369224ec75d8cc11bf305c2d17
radhasahu916 <radhasahu5657@gmail.com>
2026-02-20 22:53:28 +0530

    Add files via upload

---
+++ b3.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+int c,average;
+cout<<"enter three number";
+cin>>a>>b>>c;
+average=(a+b+c)/3;
+cout<<"average"<<average;
+return 0;
+}
