d5cafbfa69670562afa621e22da440a2a6808a93
radhasahu916 <radhasahu5657@gmail.com>
2026-02-24 23:25:17 +0530

    Add files via upload

---
+++ b11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int m1,m2,m3,total,percentage;
+cout<<"enter three number";
+cin>>m1>>m2>>m3;
+total=m1+m2+m3;
+
+percentage=(total/300)*100;
+cout<<"total="<<total<<endl;
+ cout<<"percentage"<<percentage<<"%";
+ return 0;
+ }
