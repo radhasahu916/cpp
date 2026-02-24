53a8836f0a80e9ad63bba604a1360df52f3b223b
radhasahu916 <radhasahu5657@gmail.com>
2026-02-24 23:23:45 +0530

    Add files via upload

---
+++ b7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+float a=10.5;
+int b=5;
+float result;
+cout<<"enter two number";
+cin>>a>>b;
+
+result=(a+b)/2;
+cout<<"result"<<result;
+return 0;
+
+
+}
