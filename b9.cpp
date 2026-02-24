321ee110e7cec46b22323c87e8557cec52014a1e
radhasahu916 <radhasahu5657@gmail.com>
2026-02-24 23:24:22 +0530

    Add files via upload

---
+++ b9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+char ch;
+cout<<"character";
+cin>>ch;
+if(" ch||'a', ch||'e', ch||'i', ch||'o', ch||'u'");{
+cout<<"vowels";
+}
+ if else
+ {cout<<"consonant";
+}else{
+    cout<<"invalide";
+}
+
+return 0;
+
+}
