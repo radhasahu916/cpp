0a9d7419b94b1540d2e015b465e44fdf98ec8e25
radhasahu916 <radhasahu5657@gmail.com>
2026-02-25 22:53:39 +0530

    Add files via upload

---
+++ b16.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool Result;
+
+    Result = (25 / 5 + 3 * 2 <= 15) || (!(8 - 3 > 10));
+
+    cout << "Final Result: " << Result << endl;
+
+    return 0;
+}
