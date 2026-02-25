634a430bae48c460e7be7c1d4645144430d29c3b
radhasahu916 <radhasahu5657@gmail.com>
2026-02-25 22:52:30 +0530

    Add files via upload

---
+++ b14.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2,quated,remainder;
+cout<<"enter two integer";
+cin>>num1>>num2;
+
+quated=num1/num2;
+remainder=num1%num2;
+cout<<"quated"<<quated<<endl;
+cout<<"remainder"<<remainder;
+return 0;
+
+
+}
