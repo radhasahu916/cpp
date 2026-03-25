b89f2512a44816011bf36ef49830c3fc101135b8
radhasahu916 <radhasahu5657@gmail.com>
2026-03-26 00:07:16 +0530

    Add files via upload

---
+++ Asign3,16.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 123, sum = 0;
+
+    while(n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    cout << "Sum = " << sum;
+}
