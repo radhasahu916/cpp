f32b6cad21e0bf8163f27ce8bf3be94073c6a35a
radhasahu916 <radhasahu5657@gmail.com>
2026-03-26 00:04:33 +0530

    Add files via upload

---
+++ Asign3,11.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i, j, sum;
+
+    for(i = 1; i <= 500; i++) {
+        sum = 0;
+        for(j = 1; j < i; j++) {
+            if(i % j == 0)
+                sum += j;
+        }
+        if(sum == i)
+            cout << i << " ";
+    }
+}
