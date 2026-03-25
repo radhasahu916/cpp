4b8061835069ce7774b19920c315e070ade56005
radhasahu916 <radhasahu5657@gmail.com>
2026-03-26 00:05:22 +0530

    Add files via upload

---
+++ Asign3,13.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n,fact=1;
+cout<<"enter the number";
+cin>>n;
+for( int i=1; i<=n;i++){
+    fact *=i;
+}
+cout<<"factorial="<<fact<<endl;
+
+
+}
